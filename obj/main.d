e178b7b012fd091632530989004a7e9d6b3908ed
buxiaoyang <buxiaoyang@gmail.com>
2016-03-03 17:34:06 +0800

    修改目录结构，未下载验证

---
+++ obj/main.d
@@ -0,0 +1,74 @@
+obj/main.o: main.c c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  c:/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h _PinsValues.h \
+  _Matrix.h _DS18B20.h _EEPROM.h _Display.h _TWI.h _PCF8563.h _Voice.h \
+  _Key.h _Dimming.h _Timers.h _Infrared.h
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+c:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+c:/winavr-20100110/lib/gcc/../../avr/include/avr/eeprom.h:
+
+_PinsValues.h:
+
+_Matrix.h:
+
+_DS18B20.h:
+
+_EEPROM.h:
+
+_Display.h:
+
+_TWI.h:
+
+_PCF8563.h:
+
+_Voice.h:
+
+_Key.h:
+
+_Dimming.h:
+
+_Timers.h:
+
+_Infrared.h:
